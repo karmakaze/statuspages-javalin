608d8f18954efe7fe7f9550e502119ad428b5978
Keith Kim <karmakaze@users.noreply.github.com>
2018-08-28 00:54:35 -0400

    Perhaps more idomatic

--- src/main/java/org/keithkim/statuspages/StatusPagesApp.kt
+++ src/main/java/org/keithkim/statuspages/StatusPagesApp.kt
@@ -5,7 +5,7 @@ import io.javalin.Javalin
 fun main(args: Array<String>) {
     val app = Javalin.create().start(7000)
 
-    app.get("/") {
-        ctx -> ctx.result("Hello World")
+    app.get("/") { ctx ->
+        ctx.result("Hello World")
     }
 }
