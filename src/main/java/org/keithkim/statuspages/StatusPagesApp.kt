70dced7dba37d15aada7abb41747d47997e3d239
Keith Kim <karmakaze@users.noreply.github.com>
2018-08-28 00:53:37 -0400

    Sanely formatted, 'nuf with the cutesy one-liners

--- src/main/java/org/keithkim/statuspages/StatusPagesApp.kt
+++ src/main/java/org/keithkim/statuspages/StatusPagesApp.kt
@@ -4,5 +4,8 @@ import io.javalin.Javalin
 
 fun main(args: Array<String>) {
     val app = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+
+    app.get("/") {
+        ctx -> ctx.result("Hello World")
+    }
 }
