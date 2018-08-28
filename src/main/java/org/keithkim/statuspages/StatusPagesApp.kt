33a8a87d6720c49666682e144d73cca4105be08e
Keith Kim <keith@keithkim.org>
2018-08-28 00:52:13 -0400

    Minimal 'Hello world' Javalin/Kotlin web app

---
+++ src/main/java/org/keithkim/statuspages/StatusPagesApp.kt
@@ -0,0 +1,8 @@
+package org.keithkim.statuspages;
+
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val app = Javalin.create().start(7000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
+}
